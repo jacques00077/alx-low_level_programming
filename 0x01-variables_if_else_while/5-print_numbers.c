23f11d89b7ae174bc80e10db14384d2349239b22
jacques00077 <jacquesappiagyei@gmail.com>
2023-07-14 18:15:05 -0700

    variable 3

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - A program to prints numbers from 0 to 9
+ * Return 0
+ */
+
+int main(void)
+{
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		printf("%d", j);
+	}
+	putchar('\n');
+	return (0);
+}
