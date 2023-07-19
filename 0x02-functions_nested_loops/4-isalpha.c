eb380fbeb18e11ba621aaecd07599bacc08a0bf3
jacques00077 <jacquesappiagyei@gmail.com>
2023-07-18 21:24:46 -0700

    final change

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,6 +2,7 @@
 
 /**
  * _isalpha - New Entry point
+ * @c: alpabet to check for
  * Return: 1 if c is a letter, 0 otherwise
  */
 int _isalpha(int c)
--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,6 +2,7 @@
 
 /**
  * _abs - function that computes the absolute value of an integer
+ * @n: the int to check
  * Return: the absolute value of int
  */
 int _abs(int x)
